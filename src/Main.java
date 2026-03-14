5aafff3fc4b83a1f883f0b82c6fb05c3f7edcaab
white_sweatshirt <wawer.franciszek@gmail.com>
2026-03-14 17:29:11 +0100

    first program

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("huj w dupie: ");
+        System.out.print("happens to be: ");
+        System.out.println((10.32+32*10)/2);
+    }
+
+}
