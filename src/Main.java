62faf0cafc2205d90373101c88af0c14b9c85b1c
white_sweatshirt <wawer.franciszek@gmail.com>
2026-03-14 18:59:45 +0100

    commit with jetbrains toolkit

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,12 @@ public class Main
         System.out.println("huj w dupie: ");
         System.out.print("happens to be: ");
         System.out.println((10.32+32*10)/2);
+        System.out.println(10^2);
+        double a=889;
+        System.out.print("ddad="+a);
+
+        for(int i=32;i<141;i++)
+            ;
     }
 
 }
